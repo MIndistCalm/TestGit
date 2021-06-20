3c2af54fb960155bd5e677e843db6ce1d0b4e4d8
MindistCalm <suxinin5@yandex.ru>
2021-06-20 13:20:09 +0700

    added pypyodbc

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from tkinter import *
+import pypyodbc
+
+conn = pypyodbc.connect(
+    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\suxin\OneDrive\Documents\CafeDataBase.accdb;')
+cursor = conn.cursor()
+cursor.execute('select * from Смета')
+
+for row in cursor.fetchall():
+    print(row)
 
 root = Tk()
-
-a = "Fuck You "
-print(a * 10)
 
 root.mainloop()
